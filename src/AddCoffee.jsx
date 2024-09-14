c82f7bb2c0b535eb868389ba193df2a0fee68643
rifu28 <rifatmeumeu28@gmail.com>
2024-09-14 23:50:25 +0600

    input

--- src/AddCoffee.jsx
+++ src/AddCoffee.jsx
@@ -2,6 +2,13 @@ const AddCoffee = () => {
   return (
     <div>
       <h2>Add coffee</h2>
+      <form>
+        <input
+          type="text"
+          placeholder="Type here"
+          className="input input-bordered w-full max-w-xs"
+        />
+      </form>
     </div>
   );
 };
