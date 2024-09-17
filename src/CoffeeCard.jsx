37ba9d525c1b0efc2293255f6ab65e624636567a
rifu28 <rifatmeumeu28@gmail.com>
2024-09-17 19:09:31 +0600

    edit

--- src/CoffeeCard.jsx
+++ src/CoffeeCard.jsx
@@ -1,6 +1,39 @@
+import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
+
 const CoffeeCard = ({ coffee }) => {
-  const { name, quantity, supplier, taste, category, photo } = coffee;
-  console.log(photo);
+  const { _id, name, quantity, supplier, taste, category, photo } = coffee;
+
+  const handleDelete = (_id) => {
+    console.log(_id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/coffee/${_id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data.deletedCount > 0) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your Coffee has been deleted.",
+                icon: "success",
+              });
+            }
+          });
+      }
+    });
+  };
+
   return (
     <div>
       <div className="card bg-base-100 w-96 mx-auto shadow-xl">
@@ -19,8 +52,12 @@ const CoffeeCard = ({ coffee }) => {
           </div>
           <div className="btn-group mt-5 space-x-4 text-center">
             <button className="btn">view</button>
-            <button className="btn">edit</button>
-            <button className="btn">x</button>
+            <Link to={`updateCoffee/${_id}`}>
+              <button className="btn">edit</button>
+            </Link>
+            <button onClick={() => handleDelete(_id)} className="btn">
+              x
+            </button>
           </div>
         </div>
       </div>
